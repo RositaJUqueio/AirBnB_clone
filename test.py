b3c034cd6087735748de478b5084f00eb799da0c
Kalunnenna1 <nnennakalualie@yahoo.com>
2022-09-03 14:36:49 +0000

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import re
+
+pattern = re.compile(r"(\w+)\.(\w+)\(((\"[\w|-]+\"),?\s?(\"\w+\")?,?\s?(\"?\w+\"?)?)?\)")
+
+
+while True:
+    line = input("(hbnb)$ ")
+    match = re.search(pattern, line)
+    print(match)
+    if match:
+        for i in match.groups():
+            print(i)
