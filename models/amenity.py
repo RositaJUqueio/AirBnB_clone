91e487d48691af9b8fbb7258d19b394b9b947565
Rosita Jose Uqueio <101433677+RositaJUqueio@users.noreply.github.com>
2023-10-10 22:44:16 +0200

    updating amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Representz an amenity class.
+    """Represents the amenity class.
 
     Attributes:
         name: Name of the amenity.
